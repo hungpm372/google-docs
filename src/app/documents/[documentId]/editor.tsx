d8a14d59b016cc97abdb6b3baa0a60a86ef86305
Phan Minh Hung <hungpm372@gmail.com>
2025-04-02 02:15:01 +0700

    Add link support to editor; implement link button in toolbar and update styles

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -5,6 +5,7 @@ import { Color } from '@tiptap/extension-color'
 import FontFamily from '@tiptap/extension-font-family'
 import Highlight from '@tiptap/extension-highlight'
 import Image from '@tiptap/extension-image'
+import Link from '@tiptap/extension-link'
 import Table from '@tiptap/extension-table'
 import TableCell from '@tiptap/extension-table-cell'
 import TableHeader from '@tiptap/extension-table-header'
@@ -55,6 +56,11 @@ export const Editor: FC = () => {
     immediatelyRender: false,
     extensions: [
       StarterKit,
+      Link.configure({
+        openOnClick: true,
+        autolink: true,
+        defaultProtocol: 'https'
+      }),
       Color,
       Highlight.configure({
         multicolor: true
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
+import { Input } from '@/components/ui/input'
 import { Separator } from '@/components/ui/separator'
 import { cn } from '@/lib/utils'
 import { useEditorStore } from '@/store/use-editor-store'
@@ -14,6 +16,7 @@ import {
   ChevronDownIcon,
   HighlighterIcon,
   ItalicIcon,
+  Link2Icon,
   ListTodoIcon,
   LucideIcon,
   MessageSquarePlusIcon,
@@ -24,8 +27,42 @@ import {
   UnderlineIcon,
   Undo2Icon
 } from 'lucide-react'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { type ColorResult, SketchPicker } from 'react-color'
+
+const LinkButton = () => {
+  const { editor } = useEditorStore()
+  const [value, setValue] = useState<string>('')
+
+  const handleChange = (href: string) => {
+    editor?.chain().focus().extendMarkRange('link').setLink({ href }).run()
+    setValue('')
+  }
+
+  return (
+    <DropdownMenu
+      onOpenChange={(open) => {
+        if (open) {
+          setValue(editor?.getAttributes('link').href || '')
+        }
+      }}
+    >
+      <DropdownMenuTrigger asChild>
+        <button className='h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm'>
+          <Link2Icon className='size-4' />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='p-2.5 flex items-center gap-x-2'>
+        <Input
+          placeholder='https://example.com'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button onClick={() => handleChange(value)}>Apply</Button>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 
 const HighlightColorButton = () => {
   const { editor } = useEditorStore()
@@ -510,6 +547,8 @@ export const Toolbar: FC = () => {
       ))}
       <TextColorButton />
       <HighlightColorButton />
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+      <LinkButton />
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
       {sections[2].map((item) => (
         <ToolbarButton key={item.label} {...item} />
