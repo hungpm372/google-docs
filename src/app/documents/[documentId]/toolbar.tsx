411d5096855372bab82ff15f48d856cee18cb2a1
Phan Minh Hung <hungpm372@gmail.com>
2025-04-02 13:40:29 +0700

    Add text alignment support to editor; implement alignment buttons in toolbar

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -20,6 +20,10 @@ import { cn } from '@/lib/utils'
 import { useEditorStore } from '@/store/use-editor-store'
 import { type Level } from '@tiptap/extension-heading'
 import {
+  AlignCenterIcon,
+  AlignJustifyIcon,
+  AlignLeftIcon,
+  AlignRightIcon,
   BoldIcon,
   ChevronDownIcon,
   HighlighterIcon,
@@ -40,6 +44,60 @@ import {
 } from 'lucide-react'
 import React, { FC, useState } from 'react'
 import { type ColorResult, SketchPicker } from 'react-color'
+
+const AlignButton = () => {
+  const { editor } = useEditorStore()
+
+  const alignments = [
+    {
+      label: 'Align Left',
+      value: 'left',
+      icon: AlignLeftIcon
+    },
+    {
+      label: 'Align Center',
+      value: 'center',
+      icon: AlignCenterIcon
+    },
+    {
+      label: 'Align Right',
+      value: 'right',
+      icon: AlignRightIcon
+    },
+    {
+      label: 'Align Justify',
+      value: 'justify',
+      icon: AlignJustifyIcon
+    }
+  ]
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className='h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm'>
+          <AlignLeftIcon className='size-4' />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='p-1 flex flex-col gap-y-1'>
+        {alignments.map(({ label, value, icon: Icon }) => (
+          <button
+            key={value}
+            onClick={() => editor?.chain().focus().setTextAlign(value).run()}
+            type='button'
+            aria-label={label}
+            className={cn(
+              'flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80',
+              editor?.isActive({ textAlign: value }) && 'bg-neutral-200/80'
+            )}
+          >
+            <Icon className='size-4' />
+            <span className='text-sm'>{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 
 const ImageButton = () => {
   const { editor } = useEditorStore()
@@ -636,6 +694,7 @@ export const Toolbar: FC = () => {
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
       <LinkButton />
       <ImageButton />
+      <AlignButton />
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
       {sections[2].map((item) => (
         <ToolbarButton key={item.label} {...item} />
