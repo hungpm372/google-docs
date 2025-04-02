2e0fb4d1379981e446fb430a4bd9c876c1fc9173
Phan Minh Hung <hungpm372@gmail.com>
2025-04-02 22:45:34 +0700

    Add navbar component with document input and menu options

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { Editor } from './editor'
+import { Navbar } from './navbar'
 import { Toolbar } from './toolbar'
 
 interface DocumentIdPageProps {
@@ -12,8 +13,13 @@ const DocumentIdPage: FC<DocumentIdPageProps> = async ({ params }) => {
 
   return (
     <div className='min-h-screen bg-[#fafbfd]'>
-      <Toolbar />
-      <Editor />
+      <div className='flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#fafbfd] print:hidden'>
+        <Navbar />
+        <Toolbar />
+      </div>
+      <div className='pt-[114px] print:pt-0'>
+        <Editor />
+      </div>
     </div>
   )
 }
