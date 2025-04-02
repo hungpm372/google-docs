4438e15b839029674d63962da577e68207ca703f
Phan Minh Hung <hungpm372@gmail.com>
2025-04-02 17:52:42 +0700

    Add ruler component to editor; implement draggable margins for layout adjustment

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,6 +21,7 @@ import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { FC } from 'react'
 import ImageResize from 'tiptap-extension-resize-image'
+import { Ruler } from './ruler'
 
 export const Editor: FC = () => {
   const { setEditor } = useEditorStore()
@@ -112,6 +113,7 @@ export const Editor: FC = () => {
 
   return (
     <div className='w-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible'>
+      <Ruler />
       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent editor={editor} />
       </div>
