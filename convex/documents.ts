9ae543722fd2616989eca24237e0041b4501e03f
Phan Minh Hung <hungpm372@gmail.com>
2025-04-03 17:35:30 +0700

    Add search functionality to document retrieval; update query to support search parameters and user authentication

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,5 +1,5 @@
-import { ConvexError, v } from 'convex/values'
 import { paginationOptsValidator } from 'convex/server'
+import { ConvexError, v } from 'convex/values'
 import { mutation, query } from './_generated/server'
 
 export const create = mutation({
@@ -22,9 +22,26 @@ export const create = mutation({
 })
 
 export const get = query({
-  args: { paginationOpts: paginationOptsValidator },
-  handler: async (ctx, args) => {
-    return await ctx.db.query('documents').paginate(args.paginationOpts)
+  args: { paginationOpts: paginationOptsValidator, search: v.optional(v.string()) },
+  handler: async (ctx, { paginationOpts, search }) => {
+    const user = await ctx.auth.getUserIdentity()
+
+    if (!user) {
+      throw new ConvexError('Unauthenticated user')
+    }
+
+    if (search)
+      return await ctx.db
+        .query('documents')
+        .withSearchIndex('search_title', (q) =>
+          q.search('title', search).eq('ownerId', user.subject)
+        )
+        .paginate(paginationOpts)
+
+    return await ctx.db
+      .query('documents')
+      .withIndex('by_owner_id', (q) => q.eq('ownerId', user.subject))
+      .paginate(paginationOpts)
   }
 })
 
