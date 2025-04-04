0fc44e367ff66df2fd0d2aeb771e0629afb2438c
Phan Minh Hung <hungpm372@gmail.com>
2025-04-04 22:30:53 +0700

    Add context menu for table operations in document editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,5 +1,15 @@
 'use client'
 
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger
+} from '@/components/ui/context-menu'
+import { Separator } from '@/components/ui/separator'
 import { FontSizeExtension } from '@/extensions/font-size'
 import { LineHeightExtension } from '@/extensions/line-height'
 import { useEditorStore } from '@/store/use-editor-store'
@@ -19,7 +29,9 @@ import TextStyle from '@tiptap/extension-text-style'
 import Underline from '@tiptap/extension-underline'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import { PlusIcon, Trash2Icon } from 'lucide-react'
 import { FC } from 'react'
+import { AiOutlineMergeCells, AiOutlineSplitCells } from 'react-icons/ai'
 import ImageResize from 'tiptap-extension-resize-image'
 import { Ruler } from './ruler'
 
@@ -107,9 +119,102 @@ export const Editor: FC<EditorProps> = ({ initialContent, onChange }) => {
   return (
     <div className='w-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible'>
       <Ruler />
-      <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
-        <EditorContent editor={editor} />
-      </div>
+      <ContextMenu>
+        <ContextMenuTrigger>
+          <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
+            <EditorContent editor={editor} />
+          </div>
+        </ContextMenuTrigger>
+        <ContextMenuContent className='w-[300px] py-2'>
+          <ContextMenuSub>
+            <ContextMenuSubTrigger>
+              <PlusIcon className='mr-2 size-4' /> Chèn bảng
+            </ContextMenuSubTrigger>
+            <ContextMenuSubContent className='w-[200px] py-2'>
+              {Array.from({ length: 5 }, (_, i) => (
+                <ContextMenuItem
+                  key={i}
+                  onClick={() =>
+                    editor
+                      ?.chain()
+                      .focus()
+                      .insertTable({ rows: i + 1, cols: i + 1, withHeaderRow: false })
+                      .run()
+                  }
+                  disabled={!editor?.can().insertTable({ rows: i + 1, cols: i + 1 })}
+                >
+                  <PlusIcon className='mr-2 size-4' />
+                  {i + 1} x {i + 1}
+                </ContextMenuItem>
+              ))}
+            </ContextMenuSubContent>
+          </ContextMenuSub>
+          <Separator className='my-2' />
+          <ContextMenuItem
+            onClick={() => editor?.chain().focus().toggleHeaderRow().run()}
+            disabled={!editor?.can().toggleHeaderRow()}
+          >
+            <PlusIcon className='mr-2 size-4' /> Chèn hàng tiêu đề
+          </ContextMenuItem>
+          <Separator className='my-2' />
+          <ContextMenuItem
+            onClick={() => editor?.chain().focus().addRowBefore().run()}
+            disabled={!editor?.can().addRowBefore()}
+          >
+            <PlusIcon className='mr-2 size-4' /> Chèn hàng bên trên
+          </ContextMenuItem>
+          <ContextMenuItem
+            onClick={() => editor?.chain().focus().addRowAfter().run()}
+            disabled={!editor?.can().addRowAfter()}
+          >
+            <PlusIcon className='mr-2 size-4' /> Chèn hàng bên dưới
+          </ContextMenuItem>
+          <ContextMenuItem
+            onClick={() => editor?.chain().focus().addColumnBefore().run()}
+            disabled={!editor?.can().addColumnBefore()}
+          >
+            <PlusIcon className='mr-2 size-4' /> Chèn cột vào bên trái
+          </ContextMenuItem>
+          <ContextMenuItem
+            onClick={() => editor?.chain().focus().addColumnAfter().run()}
+            disabled={!editor?.can().addColumnAfter()}
+          >
+            <PlusIcon className='mr-2 size-4' /> Chèn cột vào bên phải
+          </ContextMenuItem>
+          <Separator />
+          <ContextMenuItem
+            onClick={() => editor?.chain().focus().deleteRow().run()}
+            disabled={!editor?.can().deleteRow()}
+          >
+            <Trash2Icon className='mr-2 size-4' /> Xóa hàng
+          </ContextMenuItem>
+          <ContextMenuItem
+            onClick={() => editor?.chain().focus().deleteColumn().run()}
+            disabled={!editor?.can().deleteColumn()}
+          >
+            <Trash2Icon className='mr-2 size-4' /> Xóa cột
+          </ContextMenuItem>
+          <ContextMenuItem
+            onClick={() => editor?.chain().focus().deleteTable().run()}
+            disabled={!editor?.can().deleteTable()}
+          >
+            <Trash2Icon className='mr-2 size-4' /> Xóa bảng
+          </ContextMenuItem>
+          <Separator />
+          <ContextMenuItem
+            onClick={() => editor?.chain().focus().mergeCells().run()}
+            disabled={!editor?.can().mergeCells()}
+          >
+            <AiOutlineMergeCells className='mr-2 size-4' /> Hợp nhất ô
+          </ContextMenuItem>
+          <ContextMenuItem
+            onClick={() => editor?.chain().focus().splitCell().run()}
+            disabled={!editor?.can().splitCell()}
+          >
+            <AiOutlineSplitCells className='mr-2 size-4' /> Chia ô
+          </ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
     </div>
   )
 }
